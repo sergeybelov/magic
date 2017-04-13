7ef5ba3e0ae173c3926527c8c7bbf00cfae1bf01
sergeybelov <sergeybelov@users.noreply.github.com>
2017-04-13 18:05:17 +0300

    Add files via upload

--- Получить плохие телефоны.py
+++ Получить плохие телефоны.py
@@ -7,22 +7,42 @@ Created on Wed Apr 12 17:37:38 2017
 
 import pandas as pd
 
-data=pd.read_csv('1028258_2016-12-01_00_00_00_2017-01-31_23_59_59_58068.csv',
-                 delimiter=';',encoding='windows-1251',
+#ghjwtlehf чтения файла нужного формата
+def read_csv_sms(file_name):
+    return pd.read_csv(file_name,
+                 delimiter='\t',encoding='windows-1251',
                  usecols=['phone','submission_date','gate_status'],
                  dtype={'phone': str})#Читаем файло
 
+data=read_csv_sms('1028258_2017-01-01_00_00_00_2017-03-03_23_59_59_59888.csv')
+
+
 #предобработка
-data.send_date=data.submission_date.map(lambda arg: pd.to_datetime(arg, format='%d.%m.%Y %H:%M', errors='raise'))#Преобразуем к типу дата
+data['datetime']=data.submission_date.map(pd.to_datetime).dt.date# lambda arg: pd.to_datetime(arg, format='%d.%m.%Y %H:%M', errors='raise')).dt.date#Преобразуем к типу дата
+data.drop('submission_date',inplace=True, axis=1)#удаляем ненужную колонку
 data.gate_status=data.gate_status.map(
         lambda val: 1 if val in ['Доставлено','DELIVRD'] else 0).astype(int) #преобразуем к бинарному значению. Кому доставлено это статус "Доставлено" и Deliverd. Остальные это не доставленные.
+tmp=data.groupby(['phone','datetime'])['gate_status'].max()#считаем статус с точностью до дня, с приоритетом доставлено
+data=pd.DataFrame(data=tmp.reset_index().values)#переводим индекс обратно в колонки
+data.columns=['phone','datetime','gate_status']
+del tmp
 
-print('Размер исходной таблицы ',data.shape)
-data.drop_duplicates(inplace=True)#убираем дубликаты по всем колонкам
-print('Размер таблицы после исключения повторений',data.shape)
-data.sort_values('submission_date',inplace=True, ascending=True)#сортировка по дате
+#обработка
+data.sort_values('datetime',inplace=True, ascending=False)#сортировка по дате
+data['count'] = data.groupby('phone')['datetime'].cumcount()#Нумеруем телефоны в порядке возрастания записей
 
-data['count'] = data.groupby('phone').cumcount() + 1#Нумеруем телефоны в порядке возрастания
-phones=data[data['count']<=3].groupby('phone')['gate_status'].sum()#берем первые три записи по телефонам
-bad_phones=phones[phones!=3]#отбираем те у которых были пропуски в доставке
+#выборка плохих телефонов по размеченным данным
+phones_count=data[data['count']<3].groupby('phone')['phone'].count()#отправок должно быть не менее 3х, поэтому считаем отправки
+phones=data[data['count']<3].groupby('phone')['gate_status'].sum()#берем первые три записи по телефонам
+total=pd.concat([phones_count,phones], axis=1)#соединяем таблицы
+del phones_count,phones
+
+bad_phones=total[(total['phone']==3) & (total['gate_status']==0)].index#отбираем те у которых три раза подряд статус отправки был недоставлено
+print('Количество плохих номеров',len(bad_phones))
+
+#пишем в файл
+with open('bad_phones.csv', 'w') as file_handler:
+    file_handler.write("Phones\n")
+    for item in bad_phones:
+        file_handler.write("{}\n".format(item))
 
